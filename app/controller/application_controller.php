028ef0085d33f208a8f7535be53595273d393f23
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2009-12-23 15:18:00 -0500

    login tests added

--- app/controller/application_controller.php
+++ app/controller/application_controller.php
@@ -6,16 +6,10 @@ class ApplicationController extends \Controller {
   public function filter() {
 		$this->subdomain = $this->get_sub_domain();
     $this->user = User::find_by_username($this->subdomain);
-    if (isset($headers['User-Agent']) && strpos($headers['User-Agent'], 'PEAR') !== false) {
-      $time = DateHelper::from_db($this->user->updated_at);
-      //$this->header("Last-Modified: " . gmdate("D, d M Y H:i:s", time() - 100) . " GMT", 304);
-      
-    }
     $this->header('Content-Type: text/xml');
   }
 
 	public function get_sub_domain() {
-		$headers = apache_request_headers();
     $array = explode('.', $_SERVER['SERVER_NAME']);
     return reset($array);
 	}
--- test/functional/ChannelControllerTest.php
+++ test/functional/ChannelControllerTest.php
@@ -4,15 +4,17 @@
  */
 require_once ('nimblize/nimble_test/lib/phpunit_testcase.php');
 class ChannelControllerTest extends NimbleFunctionalTestCase {
-  public function setUp() {
-    $_SERVER['SERVER_NAME'] = 'joe.localhost';
-  }
+	
+	public function setUp() {
+		$_SERVER['SERVER_NAME'] = 'bob.localhost';
+	}
+
   public function testGetsChannelXml() {
     $this->get('index', array(), array(), array(), 'xml');
     $this->assertTemplate('index.xml');
   }
   public function testGetsChannelHtml() {
-    $this->get('index', array(), array(), array());
+    $this->get('index', array(), array(), array('user' => User::find_by_username('bob')->id));
     $this->assertTemplate('index');
   }
 }
