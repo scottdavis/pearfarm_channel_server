0e2db505e64501d88d6f579424b86939e4375049
Scott Davis <sdavis@scott-daviss-macbook-pro-2.local>
2009-12-29 14:32:59 -0500

    made analytics only load in staging and production

--- app/view/layout/application.php
+++ app/view/layout/application.php
@@ -68,14 +68,17 @@
 			<?php echo $content ?>
 			<p><?php echo link_to_back() ?></p>
 		</div>
-    <script type="text/javascript">
-    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-    </script>
-    <script type="text/javascript">
-    try {
-      var pageTracker = _gat._getTracker("UA-5410969-8");
-      pageTracker._trackPageview();
-    } catch(err) {}</script>
+		<?php if(NIMBLE_ENV == 'staging' || NIMBLE_ENV == 'production') { ?>
+   	 <script type="text/javascript">
+	    	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		    </script>
+		    <script type="text/javascript">
+		    try {
+		      var pageTracker = _gat._getTracker("UA-5410969-8");
+		      pageTracker._trackPageview();
+		    } catch(err) {}
+			</script>
+			<?php } ?>
   </body>
 </html>
