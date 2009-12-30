149ba71f1d6152a7473b1233402a4b75cf3a1c00
Scott Davis <sdavis@scott-daviss-macbook-pro-2.local>
2009-12-30 01:04:39 -0500

    fixed an issue with delete link showing up if it wasnt your package

--- app/view/version/show.php
+++ app/view/version/show.php
@@ -1,5 +1,5 @@
 <h1>Details: <?php echo $package->name ?> - <?php echo $version->version ?></h1>
 <?php echo $this->render_partial('version/_version_info.php') ?>
-<?php if($this->is_logged_in()) {?>
+<?php if($this->is_logged_in() && $this->user_id == $package->user_id) {?>
 <p><?php echo delete_link('Delete Version', url_for('VersionController', 'delete', $package->id, $version->id)) ?> </p>
 <?php } ?>
