cfa528f257077a55b0fd0776eecd2714e8c60a63
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2010-01-03 23:28:49 -0500

    added pledgie badge

--- app/controller/application_controller.php
+++ app/controller/application_controller.php
@@ -45,11 +45,11 @@ class ApplicationController extends \Controller {
 
 
 	public function set_default_side_bar() {
-		$this->latest = Package::find('all', array('limit' => '0,10', 'order' => 'created_at DESC'));
+		$this->latest = Package::find('all', array('limit' => '0,5', 'order' => 'created_at DESC'));
 		$this->sidepackage = $this->latest;
 		$this->sidebar[0]['title'] = "New Packages";
 		$this->sidebar[0]['content'] = $this->render_partial('layout/_package_list.php');
-		$this->updated = Package::find('all', array('limit' => '0,10', 'order' => 'updated_at DESC'));
+		$this->updated = Package::find('all', array('limit' => '0,5', 'order' => 'updated_at DESC'));
 		$this->sidepackage = $this->updated;
 		$this->sidebar[1]['title'] = "Recently Updated";
 		$this->sidebar[1]['content'] = $this->render_partial('layout/_package_list.php');
