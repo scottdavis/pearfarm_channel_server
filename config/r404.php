6d12f3365369892d1929028ba19a2f16cb74ee0e
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2010-01-06 18:25:39 -0500

    added error page

--- config/r404.php
+++ config/r404.php
@@ -6,25 +6,25 @@
 class r404 {
 	
 	public static function show_error_page() {
-		echo file_get_contents(__DIR__ . '/../view/layout/error.html');
+		return file_get_contents(__DIR__ . '/../view/layout/error.html');
 	}
 	
 	
   public static function GET() {
-	var_dump('HERE');
-		static::show_error_page();
+	var_dump(static::show_error_page());
+		echo static::show_error_page();
   }
   public static function PUT() {
-		static::show_error_page();
+		echo static::show_error_page();
   }
   public static function DELETE() {
-		static::show_error_page();
+		echo static::show_error_page();
   }
   public static function POST() {
-		static::show_error_page();
+		echo static::show_error_page();
   }
   public static function HEAD() {
-		static::show_error_page();
+		echo static::show_error_page();
   }
 }
 ?>
