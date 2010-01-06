a7201e0f34a9128f175e6fc4d42036fd14ae59b7
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2010-01-06 18:23:52 -0500

    added error page

--- config/r404.php
+++ config/r404.php
@@ -4,15 +4,26 @@
  * customize each function to display your error pages.
  */
 class r404 {
+	
+	public static function show_error_page() {
+		echo file_get_contents(__DIR__ . '/../view/layout/error.html');
+	}
+	
+	
   public static function GET() {
+		static::show_error_page();
   }
   public static function PUT() {
+		static::show_error_page();
   }
   public static function DELETE() {
+		static::show_error_page();
   }
   public static function POST() {
+		static::show_error_page();
   }
   public static function HEAD() {
+		static::show_error_page();
   }
 }
 ?>
