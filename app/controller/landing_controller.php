b8501e1ed3ec7be97988a2db304403d8a8f1d4af
Scott Davis <sdavis@scott-daviss-macbook-pro-2.local>
2010-01-07 17:28:21 -0500

    added new title

--- app/controller/landing_controller.php
+++ app/controller/landing_controller.php
@@ -5,6 +5,7 @@
 class LandingController extends \ApplicationController {
   public function index() {
 		$this->title = 'Making it trivially easy to create PEAR packages.';
+		Nimble::set_title($this->title);
 		$this->set_default_side_bar();
 		if($this->is_logged_in()) {
 			$this->login_user();
