d3b1cf689bcc0e32dbbadea43773c8272385403d
Scott Davis <sdavis@scott-daviss-mac-mini.local>
2010-01-09 16:43:07 -0500

    added blog link

--- lib/file_logger.php
+++ lib/file_logger.php
@@ -1,5 +1,8 @@
 #!/usr/bin/env php
 <?php
+define("CLI_RUNNER", true);
+$_SERVER['WEB_ENVIRONMENT'] = 'production';
+require_once(__DIR__ '/../config/boot.php');
 $logFile = __DIR__ . '/test.log';
 $fp      = fopen($logFile,"a+");
 $stdin   = fopen("php://stdin", "r");
@@ -14,4 +17,21 @@ while ($line = fgets($stdin))
 
 fclose($fd);
 fclose($stdin);
+
+
+
+
+  function doPackage($file_name) {
+    $path = explode(DIRECTORY_SEPARATOR, $file_name);
+    $file = array_pop($path);
+    $username = array_pop($path);
+    User::find_by_username($username);
+    
+  }
+
+
+
+
+
+
 ?>
